d79ca2e393b02193f1102cf1621d163f20685006
Yukie Kawamoto <arurukun0306@gmail.com>
2023-01-04 14:12:40 +0900

    schema

--- backend/Data/models/ordersModel.js
+++ backend/Data/models/ordersModel.js
@@ -1,28 +1,57 @@
-import {Schema,model} from "mongoose";
+import {Schema,model} from "mongoose"
 
-const userSchema=Schema({
-    name:{
-        type:String,
-        required:true
+const orderSchema=Schema({
+    orderItems:[{
+        qty:{type:Number,required:true},
+        product:{type:Schema.Types.ObjectId,required:true,ref:"ProductShra"}
+    }],
+    shippingAdress:{
+        address:{type:String,required:true},
+        country:{type:String,required:true},
+        city:{type:String,required:true},
+        postalCode:{type:Number,required:true},      
     },
-    email:{
+    paymentMethod:{
         type:String,
         required:true,
-        unique:true
     },
-    passwaord:{
-        type:String,
-        required:true
+    paymentResult:{
+        id:{type:Number},
+        status:{type:String},
+        update_time:{type:Number},
+        email_address:{type:String}
     },
-    isAdmin:{
+    taxPrice:{
+        type:Number,
+        required:true,
+    },
+    shippingPrice:{
+        type:Number,
+        required:true,
+    },
+    totalPrice:{
+        type:Number,
+        required:true,
+    },
+    isPaid:{
         type:Boolean,
         required:true,
         default:false
+    },
+    paidAt:{
+        type:Date
+    },
+    isDeliverd:{
+        type:Boolean,
+        required:true,
+        default:false
+    },
+    deliverdAt:{
+        type:Date
     }
-},{
-    timestamps:true
-})
 
-const User=model("UserShra",userSchema)
+},{timestamps:true})
 
-export default User
+const Order=model("OrderShra",orderSchema)
+
+export default Order
---
+++ backend/Data/models/productModel.js
@@ -0,0 +1,64 @@
+import {Schema,model} from "mongoose";
+
+const reviewSchema=Schema({
+    name:{type:String,required:true},
+    rating:{type:Number,required:true},
+    comment:{type:String,required:true},
+},{
+    timestamps:true
+})
+
+const productSchema=Schema({
+    user:{
+        type:Schema.Types.ObjectId,
+        required:true,
+        ref:"UserShra"
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+        
+    },
+    brand:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:Boolean,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    reviews:[reviewSchema],
+    rating:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    numReview:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    countInStock:{
+        type:Number,
+        required:true,
+    },
+
+},{
+    timestamps:true
+})
+
+const Product=model("ProductShra",productSchema)
+
+export default Product
